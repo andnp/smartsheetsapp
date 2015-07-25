7a19324d403ccb457a9ff6f307d7a00efcf79667
Andy Patterson <andy.patterson@inin.com>
2015-07-24 20:22:29 -0400

    remove node modules

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var smartsheet = require('./node/data/smartsheet');
 
-
 var app = express();
 
 smartsheet.getSheetColumns("E14024 - Jasz-Plasztic", function(columns){
