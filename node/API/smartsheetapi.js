c0be58a8b721e15e2de45f15cd1cf178541e4168
pattert <pattert101@gmail.com>
2015-07-24 22:18:49 -0400

    Added getWorkspaces from api

--- node/API/smartsheetapi.js
+++ node/API/smartsheetapi.js
@@ -56,5 +56,19 @@ module.exports = {
 				onComplete(JSON.parse(body));
 			});
 		});
-	}
+	},
+	getWorkspaces: function(onComplete){
+		fs.readFile('accesskeys.json', 'utf8', function(err,data){
+			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+			var options = {
+				url: SMARTSHEET_URL + '/workspaces',
+				headers:{
+					'Authorization' : 'Bearer ' + ACCESS_TOKEN
+				}
+			};
+			request(options, function(error, response, body){
+				onComplete(JSON.parse(body).data);
+			});
+		});
+	},
 };
