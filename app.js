2505f412875294541f3b3b518ca26e7606fccc12
Andy Patterson <andy.patterson@inin.com>
2015-07-24 23:04:21 -0400

    start populate workspace template

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ var smartsheet = require('./node/data/smartsheet');
 var smartsheetapi = require('./node/API/smartsheetapi');
 
 var app = express();
+
+smartsheetapi.getWorkspace("6090650006382468", function(workspaces){
+	console.log(workspaces);
+});
