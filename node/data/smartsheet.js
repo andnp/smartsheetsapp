157af7c1c6b55c5ebe4522f3f247ee9a40ff6846
Andy Patterson <andy.patterson@inin.com>
2015-07-24 23:37:34 -0400

    refactor code

--- node/data/smartsheet.js
+++ node/data/smartsheet.js
@@ -1,71 +1,82 @@
 var smartsheetapi = require('../API/smartsheetapi');
 
+var getSheetByName = function(sheetName, onComplete){
+	smartsheetapi.getSheets(function(sheets){
+		var id;
+		sheets.forEach(function(sheet){
+			if(sheet.name === sheetName){
+				id = sheet.id;
+				smartsheetapi.getSheet(id, function(sheetData){
+					onComplete(sheetData);
+				});
+			}
+		});
+	});
+};
+
+var getSheetColumns = function(sheetName, onComplete){
+	smartsheetapi.getSheets(function(sheets){
+		var id;
+		sheets.forEach(function(sheet){
+			if(sheet.name === sheetName){
+				id = sheet.id;
+				smartsheetapi.getColumns(id, function(columnData){
+					onComplete(columnData);
+				});
+			}
+		});
+	});
+};
+
+var getWorkspaceIdByName = function(workspaceName, onComplete){
+	smartsheetapi.getWorkspaces(function(workspaces){
+		var id;
+		workspaces.forEach(function(workspace){
+			if(workspace.name === workspaceName){
+				id = workspace.id;
+				onComplete(id);
+			}
+		});
+	});
+};
+
+var getTemplateIdByName = function(templateName, onComplete){
+	smartsheetapi.getTemplates(function(templates){
+		var id;
+		templates.forEach(function(template){
+			if(template.name === templateName){
+				id = template.id;
+				onComplete(id);
+			}
+		});
+	});
+};
+
+var getWorkspaceByName = function(workspaceName, onComplete){
+	smartsheetapi.getWorkspaces(function(workspaces){
+		var id;
+		workspaces.forEach(function(workspace){
+			if(workspace.name === workspaceName){
+				id = workspace.id;
+				smartsheetapi.getWorkspace(id, function(workspaceData){
+					onComplete(workspaceData);
+				});
+			}
+		});
+	});
+};
+
+var getSheetRows = function(sheetName, onComplete){
+	getSheetByName(sheetName, function(sheet){
+		onComplete(sheet.rows);
+	});
+};
+
 module.exports = {
-
-	getSheetByName: function(sheetName, onComplete){
-		smartsheetapi.getSheets(function(sheets){
-			var id;
-			sheets.forEach(function(sheet){
-				if(sheet.name === sheetName){
-					id = sheet.id;
-					smartsheetapi.getSheet(id, function(sheetData){
-						onComplete(sheetData);
-					});
-				}
-			});
-		});
-	},
-
-	getSheetColumns: function(sheetName, onComplete){
-		smartsheetapi.getSheets(function(sheets){
-			var id;
-			sheets.forEach(function(sheet){
-				if(sheet.name === sheetName){
-					id = sheet.id;
-					smartsheetapi.getColumns(id, function(columnData){
-						onComplete(columnData);
-					});
-				}
-			});
-		});
-	},
-
-	getWorkspaceIdByName: function(workspaceName, onComplete){
-		smartsheetapi.getWorkspaces(function(workspaces){
-			var id;
-			workspaces.forEach(function(workspace){
-				if(workspace.name === workspaceName){
-					id = workspace.id;
-					onComplete(id);
-				}
-			});
-		});
-	},
-
-	getTemplateIdByName: function(templateName, onComplete){
-		smartsheetapi.getTemplates(function(templates){
-			var id;
-			templates.forEach(function(template){
-				if(template.name === templateName){
-					id = template.id;
-					onComplete(id);
-				}
-			});
-		});
-	},
-
-	getWorkspaceByName: function(workspaceName, onComplete){
-		smartsheetapi.getWorkspaces(function(workspaces){
-			var id;
-			workspaces.forEach(function(workspace){
-				if(workspace.name === workspaceName){
-					id = workspace.id;
-					smartsheetapi.getWorkspace(id, function(workspaceData){
-						onComplete(workspaceData);
-					});
-				}
-			});
-		});
-	}
-
+	getSheetByName: getSheetByName,
+	getSheetColumns: getSheetColumns,
+	getWorkspaceIdByName: getWorkspaceIdByName,
+	getTemplateIdByName: getTemplateIdByName,
+	getWorkspaceByName: getWorkspaceByName,
+	getSheetRows: getSheetRows
 };
