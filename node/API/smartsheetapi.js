245bcb2f5204139106f2f3df9ad1dde7be28e4df
Andy Patterson <andy.patterson@inin.com>
2015-07-24 23:44:12 -0400

    refactor all code

--- node/API/smartsheetapi.js
+++ node/API/smartsheetapi.js
@@ -4,119 +4,124 @@ var fs = require('fs');
 
 var SMARTSHEET_URL = "https://api.smartsheet.com/2.0";
 
-module.exports = {
-	/**
-	 * [getSheets gets all sheets from api]
-	 * @param  {[function]} onComplete [function to be run on successful api call]
-	 */
-	getSheets: function(onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err,data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/sheets',
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN
-				}
-			};
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body).data);
-			});
+var getSheets = function(onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err,data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/sheets',
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN
+			}
+		};
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body).data);
 		});
-	},
-	/**
-	 * [getSheet gets sheet data from api]
-	 * @param  {[String]} sheetId    [The id of a sheet]
-	 * @param  {[function]} onComplete [function to be run on successful api call]
-	 */
-	getSheet: function(sheetId, onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err, data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/sheets/' + sheetId,
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN
-				}
-			}; 
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body));
-			});
-		});
-	},
-	getColumns: function(sheetId, onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err, data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/sheets/' + sheetId + "/columns/",
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN
-				}
-			}; 
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body));
-			});
-		});
-	},
-	createSheetInWorkspaceFromTemplate: function(workspaceId, sheetName, templateId, onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err, data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/workspaces/' + workspaceId + "/sheets/",
-				method: 'POST',
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN,
-					'Content-Type' : 'application/json'
-				},
-				json: {
-					name: sheetName,
-					fromId: templateId
-				}
+	});
+};
 
-			}; 
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body));
-			});
+var getSheet = function(sheetId, onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err, data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/sheets/' + sheetId,
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN
+			}
+		}; 
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body));
 		});
-	},
-	getWorkspaces: function(onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err,data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/workspaces/',
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN
-				}
-			};
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body).data);
-			});
+	});
+};
+
+var getColumns = function(sheetId, onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err, data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/sheets/' + sheetId + "/columns/",
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN
+			}
+		}; 
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body));
 		});
-	},
-	getTemplates: function(onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err,data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/templates/',
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN
-				}
-			};
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body).data);
-			});
+	});
+};
+
+var createSheetInWorkspaceFromTemplate = function(workspaceId, sheetName, templateId, onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err, data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/workspaces/' + workspaceId + "/sheets/",
+			method: 'POST',
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN,
+				'Content-Type' : 'application/json'
+			},
+			json: {
+				name: sheetName,
+				fromId: templateId
+			}
+
+		}; 
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body));
 		});
-	},
-	getWorkspace: function(workspaceId, onComplete){
-		fs.readFile('accesskeys.json', 'utf8', function(err, data){
-			var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
-			var options = {
-				url: SMARTSHEET_URL + '/workspaces/' + workspaceId,
-				headers:{
-					'Authorization' : 'Bearer ' + ACCESS_TOKEN
-				}
-			}; 
-			request(options, function(error, response, body){
-				onComplete(JSON.parse(body));
-			});
+	});
+};
+
+var getWorkspaces = function(onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err,data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/workspaces/',
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN
+			}
+		};
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body).data);
 		});
-	},
+	});
+};
+
+var getTemplates = function(onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err,data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/templates/',
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN
+			}
+		};
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body).data);
+		});
+	});
+};
+
+var getWorkspace = ,function(workspaceId, onComplete){
+	fs.readFile('accesskeys.json', 'utf8', function(err, data){
+		var ACCESS_TOKEN = JSON.parse(data).smartsheet.accesstoken;
+		var options = {
+			url: SMARTSHEET_URL + '/workspaces/' + workspaceId,
+			headers:{
+				'Authorization' : 'Bearer ' + ACCESS_TOKEN
+			}
+		}; 
+		request(options, function(error, response, body){
+			onComplete(JSON.parse(body));
+		});
+	});
+};
+
+module.exports = {
+	getSheets: getSheets,
+	getSheet: getSheet,
+	getColumns: getColumns,
+	createSheetInWorkspaceFromTemplate: createSheetInWorkspaceFromTemplate,
+	getWorkspaces: getWorkspaces,
+	getTemplates: getTemplates,
+	getWorkspace: getWorkspace
 };
