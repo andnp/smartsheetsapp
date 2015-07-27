01f2b43c09ef628ea6ac276a3375a33224e16029
Andy Patterson <andy.patterson@inin.com>
2015-07-26 21:25:08 -0400

    Start Trello API

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var smartsheet = require('./node/data/smartsheet');
 var smartsheetapi = require('./node/API/smartsheetapi');
+var trelloapi = require('./node/API/trelloapi');
 
 var app = express();
 
-smartsheetapi.getWorkspace("6090650006382468", function(workspaces){
-	console.log(workspaces);
-});
+var id = "55b3e7d0d3d1b6158b9e176b";
+
+trelloapi.getBoard(id, function(){});
